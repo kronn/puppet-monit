e3798cb194116e2980646ceaa8668400eecd829e
Matthias Viehweger <kronn@kronn.de>
2015-02-04 12:33:17 +0100

    [lint] align the => properly

--- manifests/init.pp
+++ manifests/init.pp
@@ -84,15 +84,15 @@ class monit(
   # The main configuration directory, this should have been provided by
   # the "monit" package, but we include it just to be sure.
   file { '/etc/monit':
-    ensure  => directory,
-    mode    => '0700',
+    ensure => directory,
+    mode   => '0700',
   }
 
   # The configuration snippet directory.  Other packages can put
   # *.monitrc files into this directory, and monit will include them.
   file { '/etc/monit/conf.d':
-    ensure  => directory,
-    mode    => '0700',
+    ensure => directory,
+    mode   => '0700',
   }
 
   # The main configuration file
