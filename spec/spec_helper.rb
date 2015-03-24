f2ef68d1ce925f69e906d3bd9ca7febfc0c0f914
Matthias Viehweger <kronn@kronn.de>
2015-03-24 11:39:39 +0100

    Set some default-facts to actually get the /etc/default/monit filled

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,13 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
+
+  c.default_facts = {
+    :concat_basedir         => '/var/lib/puppet/concat',
+    :kernel                 => 'Linux',
+    :operatingsystem        => 'Ubuntu',
+    :operatingsystemrelease => '12.04',
+    :osfamily               => 'Debian',
+    :lsbdistcodename        => 'precise',
+  }
 end
